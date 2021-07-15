62d38b9393e5096a760ca35a8cf6658b03a7e49b
fulara <aleksander.fular@binance.com>
2021-07-15 23:32:05 +0900

    extended main with printing all keys + A

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use std::time::Duration;
 
 fn main() {
     use KeybdKey::*;
+    install_any_key_callback(|key| {
+        println!("Pressed: {:?}", key);
+        if key == KeybdKey::A {
+            println!("And the winner is.");
+        }
+        InhibitEvent::No
+    });
     for key in [T, H, I, S, Space, A, Space, T, E, S, T].iter() {
         key.click();
     }
