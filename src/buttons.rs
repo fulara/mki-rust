080eabf9e326f5821ed31e855a119e33dc8782d7
fulara <ntszar@gmail.com>
2021-07-17 14:40:32 +0900

    Implemented mouse listening on windows.

--- src/buttons.rs
+++ src/buttons.rs
@@ -1,10 +1,11 @@
 #[derive(Copy, Clone, Ord, PartialOrd, Hash, Eq, PartialEq, Debug)]
 pub enum MouseButton {
+    // TODO: double clicks.
     Left,
     Right,
     Middle,
-    Side,
-    Extra,
+    Side,  // XBUTTON1 on win
+    Extra, // XBUTTON2 on win
     #[cfg(target_os = "linux")]
     Forward,
     #[cfg(target_os = "linux")]
