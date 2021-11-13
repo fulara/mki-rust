8e097293387665d809818eef7926816fa166fcb1
fulara <ntszar@gmail.com>
2021-11-13 08:28:08 +0100

    add pseudo Display to Mouse

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 #[cfg(target_os = "windows")] // Not sure how to detect double on linux
 #[derive(Copy, Clone, Ord, PartialOrd, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
@@ -26,4 +27,10 @@ pub enum Mouse {
     Forward,
     Back,
     Task,
+}
+
+impl fmt::Display for Mouse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("{:?}", self))
+    }
 }
